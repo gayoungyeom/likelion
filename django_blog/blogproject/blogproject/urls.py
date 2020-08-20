4a2e8ba904f90df37deb58faa536f384af8f5e93
gayoungyeom <gayoungyeom@gmail.com>
2020-08-20 21:53:57 +0900

    08.20 lecture

--- django_blog/blogproject/blogproject/urls.py
+++ django_blog/blogproject/blogproject/urls.py
@@ -16,12 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import blog.views
+import portfolio.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # blog app urls
     path('', blog.views.home, name="home"),
     path('<int:post_id>/', blog.views.detail, name="detail"),
     path('new/', blog.views.new, name="new"),
     path('create/', blog.views.create, name="create"),
     path('<int:post_id>/delete', blog.views.detail, name="delete"),
-]
+
+    # portfolio app urls
+    path('portfolio/', portfolio.views.portfolio, name="portfolio"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
