730692b74fefe6349c1fefbcb38d1f4644552cb6
gayoungyeom <gayoungyeom@gmail.com>
2020-08-25 14:07:05 +0900

    [7] CRUD - Create & Delete

--- dreamary/dreamary/page/urls.py
+++ dreamary/dreamary/page/urls.py
@@ -5,4 +5,11 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('introduce/', views.introduce, name="introduce"),
     path('profile/<int:designer_id>/', views.detail, name="detail"),
+
+    # Create - 디자이너 등록하기
+    path('new/', views.new, name="new"),
+    path('create/', views.create, name="create"),
+
+    # Delete - 정보 삭제
+    path('delete/<int:designer_id>/', views.delete, name="delete"),
 ]
--- dreamary/dreamary/page/views.py
+++ dreamary/dreamary/page/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Designer
 # Create your views here.
 
@@ -15,3 +15,27 @@ def introduce(request):
 def detail(request, designer_id):
     designer = get_object_or_404(Designer, pk=designer_id)
     return render(request, 'detail.html', {'designer': designer})
+
+
+def new(request):  # 등록하기 버튼 누르면 new.html로 이동 (form)
+    return render(request, 'new.html')
+
+
+def create(request):  # 폼 작성 후 등록버튼 누르면 디자이너 등록 (action)
+    if request.method == 'POST':  # method가 POST일 경우만 객체 생성
+        post = Designer()
+        if 'image' in request.FILES:  # 이미지가 실제로 들어온 경우만 이미지 등록
+            post.image = request.FILES['image']  # [' ']안에 값은 html에서 설정한 name
+        post.name = request.POST['name']
+        post.address = request.POST['address']
+        post.description = request.POST['description']
+
+        post.save()
+        return redirect('detail', post.id)
+
+
+def delete(request, designer_id):  # detail 페이지의 정보 삭제 버튼 누르면 삭제
+    post = get_object_or_404(Designer, pk=designer_id)
+    post.delete()
+
+    return redirect('home')
